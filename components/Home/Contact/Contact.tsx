6d9e8d8cd9bb8622c3217c532be863511dfb9b12
sengvonkh@gmail.com <sengvonkh@gmail.com>
2024-12-12 14:55:51 -0800

    change animation form

--- components/Home/Contact/Contact.tsx
+++ components/Home/Contact/Contact.tsx
@@ -6,7 +6,7 @@ const Contact = () => {
   return (
     <div className="pt-16 pb-16 bg-[#050709]">
       <div className="grid grid-cols-1 xl:grid-cols-2 w-[90%] sm:w-[80%] mx-auto items-center gap-10 mt-10">
-        <div data-aos="fade-left" data-aos-anchorpalcement="top-center">
+        <div data-aos="fade-down" data-aos-anchorpalcement="top-center">
           <ContactForm />
         </div>
         <div
